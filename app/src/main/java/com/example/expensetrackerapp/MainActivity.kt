eebd702af415ee5bc6894c67eecd912b78e41861
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-01 22:13:59 +0800

    add splash screen

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.expensetrackerapp.ui.MainApp
 import com.example.expensetrackerapp.ui.theme.ExpenseTrackerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ExpenseTrackerAppTheme {
                 // A surface container using the 'background' color from the theme
