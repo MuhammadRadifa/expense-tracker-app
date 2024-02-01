dee06efbc8df0959161f32a5a83d12e2f5c43e8f
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-01 21:26:12 +0800

    Add OnBoarding Logic

--- app/src/main/java/com/example/expensetrackerapp/ui/MainApp.kt
+++ app/src/main/java/com/example/expensetrackerapp/ui/MainApp.kt
@@ -1,17 +1,12 @@
 package com.example.expensetrackerapp.ui
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.expensetrackerapp.ui.screen.MainScreen
-import com.example.expensetrackerapp.ui.screen.expense.AddExpenseScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
+import androidx.compose.runtime.Composable
+import com.example.expensetrackerapp.ui.screen.support.OnBoardingScreen
+
 @Composable
 fun MainApp() {
     //AddExpenseScreen()
-    MainScreen()
+    //MainScreen()
+    OnBoardingScreen()
 }
--- app/src/main/java/com/example/expensetrackerapp/ui/screen/support/OnBoardingScreen.kt
+++ app/src/main/java/com/example/expensetrackerapp/ui/screen/support/OnBoardingScreen.kt
@@ -1,18 +1,38 @@
 package com.example.expensetrackerapp.ui.screen.support
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -21,11 +41,72 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.expensetrackerapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OnBoardingScreen(){
+
+    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState {
+        3
+    }
+
+    var imagePainter = remember{ 0 }
+    var heightImage = remember{ 0 }
+    var title = remember{ "" }
+    var description = remember { "" }
+
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+        HorizontalPager(state = pagerState) {
+            when(pagerState.currentPage){
+                0 ->{
+                    imagePainter = R.drawable.undraw_mobile_content_xvgr
+                    heightImage = 300
+                    title = "Welcome to Expense Tracker"
+                    description = "Easily manage your finances. Expense Tracker helps you record and manage your daily expenses effortlessly"
+                }
+                1->{
+                    imagePainter = R.drawable.stat
+                    heightImage = 200
+                    title = "Record Expenses Quickly"
+                    description = "Expense Tracker makes daily expense tracking a breeze. Just a few taps to record your transactions"
+
+                }
+                2->{
+                    imagePainter = R.drawable.real_time
+                    heightImage = 260
+                    title = "Real-time Financial Monitoring"
+                    description = "Monitor your finances instantly. Expense Tracker provides clear insights with informative graphs and reports for wise financial decisions"
+                }
+            }
+                OnBoardingItem(imagePainter = imagePainter, heightImage = heightImage, title = title, description = description)
+        }
+        if(pagerState.currentPage == 2){
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(top = 100.dp, bottom = 80.dp).padding(horizontal = 10.dp),
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.background),
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Get Started", fontSize = 20.sp, fontWeight = FontWeight.Medium)
+            }
+        }else{
+            IndicatorNavigation(pagerState,scope)
+        }
+        
+    }
+
+}
 
 @Composable
-fun OnBoardingScreen(imagePainter:Int,heightImage: Int,title:String,description:String){
+fun OnBoardingItem(imagePainter:Int,heightImage: Int,title:String,description:String){
     Column(
-        Modifier.fillMaxSize(),
+        Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
@@ -49,35 +130,30 @@ fun OnBoardingScreen(imagePainter:Int,heightImage: Int,title:String,description:
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingSecondScreen(){
-    Column(
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(
-            painter = painterResource(id = R.drawable.stat),
-            contentDescription = "First Icon",
-            Modifier.height(200.dp)
-        )
-        Spacer(Modifier.height(16.dp))
-        Text(
-            text = "Record Expenses Quickly",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(Modifier.height(8.dp))
-        Text(
-            text = "Expense Tracker makes daily expense tracking a breeze. Just a few taps to record your transactions",
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Light,
-            modifier = Modifier.fillMaxWidth(0.75f)
-        )
+fun IndicatorNavigation(pagerState: PagerState,scope:CoroutineScope){
+    Column(Modifier.padding(top = 100.dp, bottom = 80.dp)) {
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            repeat(3){
+                val selected = pagerState.currentPage == it
+                Box(
+                    Modifier
+                        .size(18.dp)
+                        .clip(shape = CircleShape)
+                        .background(color = if (selected) Color.Black else Color.Gray)
+                        .clickable {
+                            scope.launch {
+                                pagerState.animateScrollToPage(it)
+                            }
+                        }
+
+                )
+                Spacer(Modifier.width(8.dp))
+            }
+        }
     }
 }
-
-@Composable
-fun OnBoardingThirdScreen(){}
 
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -97,10 +173,6 @@ fun OnBoardingPreview(){
 //        description = "Expense Tracker makes daily expense tracking a breeze. Just a few taps to record your transactions"
 //    )
 
-    OnBoardingScreen(
-        imagePainter = R.drawable.real_time,
-        heightImage = 260,
-        title = "Real-time Financial Monitoring",
-        description = "Monitor your finances instantly. Expense Tracker provides clear insights with informative graphs and reports for wise financial decisions"
-    )
+    OnBoardingScreen()
+
 }
