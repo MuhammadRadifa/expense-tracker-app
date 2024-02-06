bafd73f5331f731840d0e0a0a057b1c169606d7d
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-06 10:14:53 +0800

    add expense entity

--- app/src/main/java/com/example/expensetrackerapp/data/Expense.kt
+++ app/src/main/java/com/example/expensetrackerapp/data/Expense.kt
@@ -1,2 +1,19 @@
 package com.example.expensetrackerapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "expense")
+data class Expense(
+    @PrimaryKey(autoGenerate = true)
+    val id:Long = 0L,
+    @ColumnInfo(name = "amount")
+    val amount:Int = 0,
+    @ColumnInfo(name = "description")
+    val description:String = "",
+    @ColumnInfo(name = "category")
+    val category:String = "",
+    @ColumnInfo(name = "date")
+    val date:String = ""
+)
